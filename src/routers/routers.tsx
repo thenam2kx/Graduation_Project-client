539610e59f2cd57bdc5b91746c1972fa292d508b
tampv14 <ygvv468@gmail.com>
2025-06-17 20:37:39 +0700

    design pages checkout

--- src/routers/routers.tsx
+++ src/routers/routers.tsx
@@ -13,9 +13,10 @@ import LayoutAccountPage from '@/pages/account/layout.account.page'
 import AddressForm from '@/pages/account/account-info/address.form'
 import BlogPage from '@/pages/blog/blog.page'
 import ProductDetail from '@/pages/productDetail/productDetail'
-import BlogDetailPage from '@/pages/blog/blogDetail.page'
+import BlogDetailPage from '@/pages/blog/blog.detail.page'
 import CartPage from '@/pages/cart/cart.page'
 import WishlistPage from '@/pages/wishlist/wishlist.page'
+import CheckoutBilling from '@/pages/checkout/checkout.billing'
 
 const Routers = () => {
   const isAuthenticated = useAppSelector((state) => state.auth.isSignin)
@@ -28,6 +29,7 @@ const Routers = () => {
           <Route path='shops' element={<ProductPage />} />
           <Route path='blogs' element={<BlogPage />} />
           <Route path='productDetail/:id' element={<ProductDetail />} />
+          <Route path='checkout' element={<CheckoutBilling />} />
           <Route path='blogs/:blogId' element={<BlogDetailPage />} />
           <Route path='account/:id' element={<LayoutAccountPage />}>
             <Route path='wishlist' element={<WishlistPage />} />
