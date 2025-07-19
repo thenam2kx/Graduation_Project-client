20702f64608c13720abbb38dd18c49ad0170eb09
Minq05 <hoangquan22022005@gmail.com>
2025-07-19 22:30:53 +0700

    update layout

--- src/pages/layout.page.tsx
+++ src/pages/layout.page.tsx
@@ -6,9 +6,9 @@ import { Outlet } from 'react-router'
 const LayoutPage = () => {
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', width: '100%' }}>
       <AppHeader />
-      <div>
+      <div className="w-full">
         <Outlet />
       </div>
       <AppFooter />
